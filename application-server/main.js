9902ec4239868cecb530b9b36a5cfe8599bf4a1f
benlowry <benlowry@outlook.com>
2020-07-23 14:03:09 -0600

    WIP

--- application-server/main.js
+++ application-server/main.js
@@ -1,9 +1,9 @@
 global.keyLength = parseInt(process.env.KEY_LENGTH || '10', 10)
 global.maxLength = parseInt(process.env.MAX_LENGTH || '9999999', 10)
 global.publicDomain = process.env.PUBLIC_DOMAIN || false
-global.dashboardServer = process.env.DASHBOARD_SERVER
-global.applicationServer = process.env.APPLICATION_SERVER
-global.applicationServerToken = process.env.APPLICATION_SERVER_TOKEN
+global.dashboardServer = global.dashboardServer || process.env.DASHBOARD_SERVER
+global.applicationServer = global.applicationServer || process.env.APPLICATION_SERVER
+global.applicationServerToken = global.applicationServerToken || process.env.APPLICATION_SERVER_TOKEN
 
 let server
 module.exports = {
