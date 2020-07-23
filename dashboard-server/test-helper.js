b628abad29bc6ed108dc912d84e7581d825ae081
benlowry <benlowry@outlook.com>
2020-07-23 09:46:50 -0600

    WIP

---
+++ dashboard-server/test-helper.js
@@ -0,0 +1,26 @@
+/* eslint-env mocha */
+let applicationServer
+
+module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
+
+before(async () => {
+  if (applicationServer) {
+    return
+  }
+  applicationServer = require('../application-server/main.js')
+  await applicationServer.start(process.env.APPLICATION_SERVER_PORT)
+  global.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
+  global.applicationServerToken = process.env.APPLICATION_SERVER_TOKEN
+})
+
+beforeEach(async () => {
+  global.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
+  global.applicationServerToken = process.env.APPLICATION_SERVER_TOKEN
+})
+
+after(async () => {
+  if (applicationServer) {
+    await applicationServer.stop()
+    applicationServer = null
+  }
+})
