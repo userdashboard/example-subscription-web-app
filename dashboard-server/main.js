2097603dd95807a38228296d8d4cf3fdb4ed4195
benlowry <benlowry@outlook.com>
2020-08-09 12:27:00 -0600

    WIP

--- dashboard-server/main.js
+++ dashboard-server/main.js
@@ -1,5 +1,2 @@
 const dashboard = require('@userdashboard/dashboard')
 dashboard.start(__dirname)
-
-const applicationServer = require('../application-server/main.js')
-applicationServer.start(process.env.APPLICATION_SERVER_PORT, global.dashboardServer)
--- dashboard-server/test-helper.js
+++ dashboard-server/test-helper.js
@@ -4,12 +4,10 @@ let applicationServer
 module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
 
 before(async () => {
-  console.log('test-helper-before....')
   if (applicationServer) {
     return
   }
   applicationServer = require('../application-server/main.js')
-  console.log('starting from before', process.env.APPLICATION_SERVER_PORT)
   await applicationServer.start(process.env.APPLICATION_SERVER_PORT, global.dashboardServer)
   global.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
   global.applicationServerToken = process.env.APPLICATION_SERVER_TOKEN
