b0c97373b78a025a62fd177d1721ffbda7dca0c7
benlowry <benlowry@outlook.com>
2020-08-16 11:13:58 -0600

    Switch to global container for test configuration

--- dashboard-server/test-helper.js
+++ dashboard-server/test-helper.js
@@ -1,15 +1,10 @@
 /* eslint-env mocha */
 global.applicationPath = __dirname
+global.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
+global.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
+global.defaultConfiguration.applicationServerToken = 'token'
 
-const SubscriptionTestHelper = module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
-SubscriptionTestHelper.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
-SubscriptionTestHelper.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
-SubscriptionTestHelper.defaultConfiguration.applicationServerToken = 'token'
-
-const TestHelper = require('@userdashboard/dashboard/test-helper.js')
-TestHelper.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
-TestHelper.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
-TestHelper.defaultConfiguration.applicationServerToken = 'token'
+module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
 
 const applicationServer = require('../application-server/main.js')
 applicationServer.start(process.env.APPLICATION_SERVER_PORT, global.dashboardServer)
