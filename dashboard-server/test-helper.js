5db58fb2b4cdc4a667a19b9625bcbcf30c5eb82d
benlowry <benlowry@outlook.com>
2020-08-18 08:51:01 -0600

    WIP

--- dashboard-server/test-helper.js
+++ dashboard-server/test-helper.js
@@ -2,6 +2,7 @@
 global.applicationPath = __dirname
 
 module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
+require('@userdashboard/dashboard/test-helper.js')
 global.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
 global.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
 global.defaultConfiguration.applicationServerToken = 'token'
