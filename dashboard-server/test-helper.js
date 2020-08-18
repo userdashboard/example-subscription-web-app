1832bdd260a3cbaa525b7faab0895b9f18a1a336
benlowry <benlowry@outlook.com>
2020-08-18 08:16:18 -0600

    WIP

--- dashboard-server/test-helper.js
+++ dashboard-server/test-helper.js
@@ -1,10 +1,11 @@
 /* eslint-env mocha */
 global.applicationPath = __dirname
+
+module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
 global.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
 global.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
 global.defaultConfiguration.applicationServerToken = 'token'
 
-module.exports = require('@userdashboard/stripe-subscriptions/test-helper.js')
 
 const applicationServer = require('../application-server/main.js')
 applicationServer.start(process.env.APPLICATION_SERVER_PORT, global.dashboardServer)
